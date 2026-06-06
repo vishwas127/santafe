d62848a3a8e8b8e657575997b8f85182fbdc3d1d
DevOps Engineer <devops@example.com>
2026-06-06 17:21:10 +0530

    fix: deploying aks cluster

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_subnet" "subnets" {
   for_each = var.subnets
 
   name                 = each.key
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = each.value.address_prefixes
 }
